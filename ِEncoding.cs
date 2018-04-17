a5ce85d7823e02eab7164c0b4106464eb2a44944
Halim <35654502+Ahmed-Halim@users.noreply.github.com>
2018-04-17 22:42:15 +0200

    Update ِEncoding.cs

--- ِEncoding.cs
+++ ِEncoding.cs
@@ -1,40 +1,52 @@
-﻿using System;
-namespace FO {
-    class Encoding {
-        
-        public static string encode(string text) {
+using System;
+namespace e
+{
+    class Program
+    {
+        public static string encode(string text)
+        {
             int start = 0, Length = text.Length;
             string ans = "";
-            for (int counter = 0; counter < Length; counter++) {
-                if (text[start] != text[counter]) {
-                    ans += (counter - start > 1) ? "*" + (counter - start).ToString() + text[start] : text[start];
+            for (int counter = 0; counter < Length; counter++)
+            {
+                if (text[start] != text[counter])
+                {
+                    ans += (counter - start > 1) ? "*" + text[start].ToString() + (counter - start).ToString() : text[start].ToString();
                     start = counter;
                 }
             }
-            ans += (Length - start > 1) ? "*" + (Length - start).ToString() + text[start] : text[start];
+
+            ans += (Length - start > 1) ? "*" + text[start].ToString() + (Length - start).ToString(): text[start].ToString();
+            
             return ans;
         }
 
-        public static string decode(string text) {
+        public static string decode(string text)
+        {
             string ans = "";
-            for (int i = 0; i < text.Length; i++) {
-                if (text[i] == '*') {
-                    ans += new string(text[i + 2], text[i + 1] - '0');
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '*')
+                {
+                    ans += new string(text[i + 1], text[i + 2] - '0');
                     i += 2;
-                } else {
+                }
+                else
+                {
                     ans += text[i];
                 }
             }
             return ans;
         }
 
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             Console.Write("Enter text to be encoded: ");
             String text = Console.ReadLine();
             String encoded = encode(text);
             String decoded = decode(encoded);
             Console.WriteLine("Encoded: " + encoded);
-            Console.WriteLine("Decoded: " + decoded); 
+            Console.WriteLine("Decoded: " + decoded);
         }
     }
 }
